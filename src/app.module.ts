1bf4aad796617e7a2b81693402b699db3018ce91
Christian Esclamado <chrisesclamado@gmail.com>
2022-09-02 14:42:30 +0800

    initial commit, implementing schema first

---
+++ src/app.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      typePaths: ['./**/*.graphql'],
+      playground: true
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb://127.0.0.1:27017/nest-graphql',
+      entities: [join(__dirname, '**/**.entity{.ts,.js}')],
+      synchronize: true,
+      useNewUrlParser: true,
+      logging: true,
+    }),
+    UserModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
